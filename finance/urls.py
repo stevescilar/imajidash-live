e88801b3716ba40870d5f55f1b22297902f709d9
stevescilar <stevemuambi@gmail.com>
2022-08-23 13:25:43 +0300

    cleaning up

--- finance/urls.py
+++ finance/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    # path to trackfinance ``
+    # path to trackfinance ``.
     path('',views.trackFinance ,name='track-finance'),
 ]
--- finance/views.py
+++ finance/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def trackFinance(request):
     
-    return render(request,'dashboard/track_finance.html')
+    return render(request,'finance/track_finance.html')
